c1a770c40e8991e69cb1ec4e6bf33bb1affc85d4
eunhak <akfmzh1207@naver.com>
2024-03-03 15:54:43 +0900

    error test

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+          <adf></adf>
         </a>
       </div>
       <h1>Vite + React</h1>
