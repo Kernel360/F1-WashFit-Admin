ae6adf247e363ee88a5c40aeffbbb6b644ffb509
eunhak <akfmzh1207@naver.com>
2024-03-04 15:45:26 +0900

    cookie setting

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>PR test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
---
+++ src/api/apiAxios.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import axios from "axios";
+
+export const apiAxios = axios.create({
+  baseURL: import.meta.env.VITE_APP_SERVER_HOST,
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 5000,
+});
